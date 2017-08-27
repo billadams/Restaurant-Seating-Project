d2f8d919fd14fa89de3ccaa81334ddb850fa845a
quentin987 <mailqt987@yahoo.com>
2017-08-27 15:13:21 -0500

    Validation changes for form

--- RestaurantSeatingProject/frmCreateTableLayout.cs
+++ RestaurantSeatingProject/frmCreateTableLayout.cs
@@ -26,20 +26,50 @@ namespace RestaurantSeatingProject {
         private void btnAddTable_Click(object sender, EventArgs e) {
             int startLeft = pnlTableLayout.Left;
             int startTop = pnlTableLayout.Top;
+            string sErrorMess = "";
+            bool bIsValid = true;
 
-            table.TableNumber = Convert.ToInt32(txtTableNumber.Text);       
-            table.NumberOfSeats = Convert.ToInt32(txtNumberOfSeats.Text);
-            table.TablePositionX = startLeft;
-            table.TablePositionY = startTop;
-            tables.Add(table);
+            try
+            {
+                table.TableNumber = Convert.ToInt32(txtTableNumber.Text);
+            }
+            catch (Exception)
+            {
 
-            Button button = new Button();
-            button.Height = 50;
-            button.Text = "Table " + Convert.ToString(table.TableNumber) 
-                + "\n" + Convert.ToString(table.NumberOfSeats) + " seats";
-            button.Location = new Point(startLeft, startTop);
-            button.Click += new EventHandler(TableHandler);
-            pnlTableLayout.Controls.Add(button);
+                bIsValid = false;
+                sErrorMess += "TableNumber is required and must be a number";
+              
+            }
+
+            try
+            {
+                table.NumberOfSeats = Convert.ToInt32(txtNumberOfSeats.Text);
+            }
+            catch (Exception)
+            {
+                bIsValid = false;
+                sErrorMess += "\nNumber of Seats is required and must be a number";
+             
+            }
+
+            if (bIsValid)
+            {
+                table.TablePositionX = startLeft;
+                table.TablePositionY = startTop;
+                tables.Add(table);
+
+                Button button = new Button();
+                button.Height = 50;
+                button.Text = "Table " + Convert.ToString(table.TableNumber)
+                    + "\n" + Convert.ToString(table.NumberOfSeats) + " seats";
+                button.Location = new Point(startLeft, startTop);
+                button.Click += new EventHandler(TableHandler);
+                pnlTableLayout.Controls.Add(button);
+            }
+            else
+            {
+                MessageBox.Show(sErrorMess, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void TableHandler(object sender, EventArgs e) {
