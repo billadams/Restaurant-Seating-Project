db89b0142e4d98a3c85ae5a9b1211ecbb9ea2789
quentin987 <mailqt987@yahoo.com>
2017-08-27 21:40:45 -0500

    Added classes

---
+++ RestaurantSeatingProject/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSeatingProject
+{
+    class Restaurant
+    {
+        //COULD add password if we need to later so people can't mess with other people's stuff
+        public string RestaurantName { get; set; }
+        public string UserName { get; set; }
+        public Restaurant()
+        {
+
+        }
+
+        public Restaurant(string sRestaurantName, string sUserName)
+        {
+            this.RestaurantName = sRestaurantName;
+            this.UserName = sUserName;
+        }
+    }
+}
