e10479bb8ec89bc4b92e5e490a3c585b5786c9bf
Renell Yonkedeh <ry604193@southeast.edu>
2017-08-30 20:57:12 -0500

    Update SQL code

--- RestaurantSeatingProject/UML/restaurantDB.sql
+++ RestaurantSeatingProject/UML/restaurantDB.sql
@@ -1,52 +1,60 @@
 CREATE TABLE customer{
 	lastName VARCHAR(30) NOT NULL,
-	groupSize VARCHAR(20) NOT NULL,
 	custTime  TIME NOT NULL,
 	custID INT NOT NULL AUTO_INCREMENT,
 	CONSTRAINT custPK  PRIMARY KEY(custID)
 };
 CREATE TABLE server{
+	firstName VARCHAR(25) NOT NULL,
+	lastName VARCHAR(30) NOT NULL,
 	serverID INT NOT NULL AUTO_INCREMENT,
-	personID INT NOT NULL,
-	break TIME NULL,
+	empNum TIME NULL,
 	CONSTRAINT serverPK PRIMARY KEY(serverID),
-	CONSTRAINT serverFK FOREIGN KEY(personID) REFERENCES persons(personID)
 };
-CREATE TABLE restTable{
-	numChairs INT NOT NULL,
-	restID INT NOT NULL,
-	tableID INT NOT NULL AUTO_INCREMENT,
-	CONSTRAINT tablePK PRIMARY KEY(tableID),
-	CONSTRAINT restFK FOREIGN KEY(restID) REFERENCES restaurant (restID)
+CREATE TABLE tables{
+	tableNum INT NOT NULL,
+	numSeat INT NOT NULL,
+	xposition INT NOT NULL,
+	yposition INT NOT NULL,
+	CONSTRAINT tablePK PRIMARY KEY(tableNum),
+	CONSTRAINT tRestFK FOREIGN KEY(restID) REFERENCES restaurants (restID)
 };
-CREATE TABLE restaurant{
-	name
-	address
-	city
-	restState
-	zip
-	restID
+CREATE TABLE restaurants{
+	name VARCHAR(25) NOT NULL,
+	address VARCHAR(30) NOT NULL,
+	city VARCHAR(25) NOT NULL,
+	restState VARCHAR(2) NOT NULL,
+	zip VARCHAR(5) NOT NULL,
+	restaurantID INT NOT NULL AUTO_INCREMENT,
+	CONSTRAINT restaurantPK PRIMARY KEY(restaurantID)
 };
-CREATE TABLE section{
-	tableID
-	restID
+CREATE TABLE sections{
+	tableNum INT NOT NULL,
+	restaurantID INT NOT NULL,
+	CONSTRAINT sectionsPK PRIMARY KEY(tablenum,restaurantID),
+	CONSTRAINT tableFK FOREIGN KEY(tableNum) REFERENCES tables (tableNum),
+	CONSTRAINT sRestFK FOREIGN KEY(restaurantID) REFERENCES restaurant (restaurantID)
+	
 };
-CREATE TABLE reservation{
-	lastName
-	theDate
-	theTime
-	groupSize
-	restID
-	reservationID
+CREATE TABLE reservations{
+	theDate DATE NOT NULL,
+	theTime TIME NOT NULL,
+	groupSize INT NOT NULL,
+	reservationID INT NOT NULL AUTO_INCREMENT,
+	CONSTRAINT reservationPK PRIMARY KEY(reservationID)
 };
-CREATE TABLE persons{
-	firstName
-	lastName
-	email
-	personID
-	restID
-};
+
 CREATE TABLE waitList{
-	reservationID
-	custID
+	waitListID INT NOT NULL AUTO_INCREMENT,
+	custID INT NOT NULL,
+	CONSTRAINT waitListPK PRIMARY KEY(waitListID),
+	CONSTRAINT wCustFK FOREIGN KEY(custID) REFERENCES customer (custID)
 };
+CREATE TABLE assignSection(
+	assignID INT NOT NULL AUTO_INCREMENT,
+	sectionNum INT NOT NULL,
+	tableNum INT NOT NULL,
+	CONSTRAINT assignPK PRIMARY KEY(assignID),
+	CONSTRAINT atableFK FOREIGN KEY(tableNum) REFERENCES tables (tableNum),
+	CONSTRAINT aSectionFK FOREIGN KEY(sectionNum) REFERENCES sections (sectionNum)
+);
