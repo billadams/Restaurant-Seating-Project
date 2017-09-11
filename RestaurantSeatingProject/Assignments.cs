49da6315a8f7400094d551ba1fba37b68fef6d73
th630236 <timh412@inebraska.com>
2017-09-11 18:11:14 -0500

    Add constructors to class Assignments (it had none)

--- RestaurantSeatingProject/Assignments.cs
+++ RestaurantSeatingProject/Assignments.cs
@@ -8,9 +8,26 @@ namespace RestaurantSeatingProject
 {
     class Assignments
     {
-        public string ServerName { get; set; }
-        public int ServerID { get; set; }
-        public int TableNumber { get; set; }
-        public int SectionNumber { get; set; }
+        public string ServerName    { get; set; }
+        public int    ServerID      { get; set; }
+        public int    TableNumber   { get; set; }
+        public int    SectionNumber { get; set; }
+
+        public Assignments()
+        {
+            ServerName ="";
+            ServerID = 0;
+            TableNumber = 0;
+            SectionNumber = 0;
+        }
+
+        public Assignments(string ServerName, int ServerID, int TableNumber, int SectionNumber)
+        {
+            this.ServerName    = ServerName;
+            this.ServerID      = ServerID;
+            this.TableNumber   = TableNumber;
+            this.SectionNumber = SectionNumber;
+        }
     }
 }
+
